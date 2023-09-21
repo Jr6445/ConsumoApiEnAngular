a56136c0b5f67fa8591d4d52fe83105782f5e69b
Jose <jose.rivera.jr6445@gmail.com>
2023-09-21 16:55:47 -0600

    Get

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; // Importa HttpClientModule
+
 
 import { AppComponent } from './app.component';
+import { BlogComponent } from './components/blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
